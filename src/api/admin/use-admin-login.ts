d8b30a0e5728fe41a8814c9e14e7930f1ecfbcbd
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-08-13 12:43:33 +0530

    added codes

--- src/api/admin/use-admin-login.ts
+++ src/api/admin/use-admin-login.ts
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom"
 import { z } from "zod"
 import { apiClient } from "../api-client"
 import { adminloginSchema } from "@/pages/admin-login"
+import Cookies from 'js-cookie';
+
 
 export const useAdminLogin = () => {
     const navigate = useNavigate()
@@ -10,7 +12,8 @@ export const useAdminLogin = () => {
         {
             mutationFn: async (loginData: z.infer<typeof adminloginSchema>) => {
                 try {
-                    await apiClient.post("auth/admin-login", loginData)
+                    const { data } = await apiClient.post("auth/admin-login", loginData)
+                    Cookies.set('access_token', data.token, { expires: 7, path: '/' });
                     navigate("/dashboard")
                 } catch (error) {
                     console.log(error);
